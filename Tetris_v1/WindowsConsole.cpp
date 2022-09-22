5e2cb5906e617456c0b84519a1c60799a856d310
ohTaesung <ts5516@naver.com>
2022-09-22 12:41:29 +0900

    메인함수 정리

--- Tetris_v1/WindowsConsole.cpp
+++ Tetris_v1/WindowsConsole.cpp
@@ -2,22 +2,17 @@
 
 using namespace std;
 
-void gameStart(const int& speed)
+int main()
 {
     Game game;
     KEYCODE key;
-    
+
     while (game.getGamestate() != GAMESTATE::END)
     {
         key = game.getInput();
-        
+
         game.gameUpdate(key);
-       
+
         game.gameRender();
     }
-}
-
-int main()
-{
-    gameStart(1);
 }
